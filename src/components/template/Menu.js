f48191ebf6af050108246d63fd19e92624b4f749
Rodrigo Avellar <roavellarm@gmail.com>
2019-10-28 17:43:33 -0300

    Update Menu

--- src/components/template/Menu.js
+++ src/components/template/Menu.js
@@ -5,8 +5,11 @@ export default () => {
   return (
     <nav>
       <Link to="/">Home</Link>
+      <Link to="/room">Room</Link>
       <Link to="/contact">Contact</Link>
       <Link to="/about">About</Link>
+      <Link to="/sign-in">Sign-in</Link>
+      <Link to="/sign-out">Sign-out</Link>
     </nav>
   )
 }
