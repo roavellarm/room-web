8f6f06652bb5ca77dca6065f5bb7fbe0a27f571f
Rodrigo Avellar <roavellarm@gmail.com>
2019-10-28 17:44:39 -0300

    Update button style

--- src/components/template/Button.js
+++ src/components/template/Button.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
-const StyledButton = styled(Button)`
-  border: none;
-  cursor: pointer;
-  color: white;
-  background: black;
-  width: 100px;
-  height: 30px;
+// const StyledButton = styled(Button)`
+//   border: none;
+//   cursor: pointer;
+//   color: white;
+//   background: black;
+//   width: 100px;
+//   height: 30px;
 
-  &:hover {
-    color: black;
-    background: white;
-  }
-`
+//   &:hover {
+//     color: black;
+//     background: white;
+//   }
+// `
+
+// export default ({ children, onClick }) => {
+//   return <StyledButton onClick={onClick}>{children}</StyledButton>
+// }
 
 export default ({ children, onClick }) => {
-  return <StyledButton onClick={onClick}>{children}</StyledButton>
+  return (
+    <Button onClick={onClick} type="submit" variant="contained" color="primary">
+      {children}
+    </Button>
+  )
 }
