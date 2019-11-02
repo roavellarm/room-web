3fa71f3dc2e8cedc0a432a23ffdc8f698d59d70e
Rodrigo Avellar <roavellarm@gmail.com>
2019-11-02 17:29:25 -0200

    Add axios interceptor to increase security

--- src/api/index.js
+++ src/api/index.js
@@ -2,4 +2,16 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:5000'
 
+axios.interceptors.request.use(
+  config => config,
+  error => {
+    if (error.response.status === 401) {
+      localStorage.removeItem('isAuthenticated')
+      // window.location = '/'
+    } else {
+      return Promise.reject(error)
+    }
+  },
+)
+
 export default axios
