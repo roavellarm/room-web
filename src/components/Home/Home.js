1661efa2d905e993f812b91bad9ec29f4dbf728e
Rodrigo Avellar <roavellarm@gmail.com>
2019-11-09 18:56:22 -0200

    Jitsi studies

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,36 @@
 import React from 'react'
-import { Typography, Grid } from '@material-ui/core'
+import { Grid, makeStyles, Container, CssBaseline } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  frame: {
+    flex: 3,
+    width: '100%',
+    height: '100vh',
+  },
+}))
 
 export default () => {
+  const classes = useStyles()
+
   return (
-    <>
-      <Typography component="h1" variant="h3">
-        Home
-      </Typography>
-      <Grid container>
-        
-      </Grid>
-    </>
+    <Grid container component="main" align="center" className={classes.root}>
+      <CssBaseline />
+      <Container width="100%">
+        <Grid item square>
+          HOME
+        </Grid>
+        <Grid item square>
+          <iframe
+            className={classes.frame}
+            id="Teste"
+            title="teste"
+            src="https://meet.jit.si/098098098"
+          ></iframe>
+        </Grid>
+      </Container>
+    </Grid>
   )
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,21 @@
 import React from 'react'
 import Home from '../components/Home'
+// import myHtml from './'
+
+// const domain = 'meet.jit.si'
+// const options = {
+//   roomName: 'JitsiMeetAPIExample',
+//   width: 700,
+//   height: 500,
+//   parentNode: undefined,
+//   configOverwrite: {},
+//   interfaceConfigOverwrite: {
+//     filmStripOnly: true,
+//   },
+// }
+// const api = new JitsiMeetExternalAPI(domain, options)
+
+// const myHtml = document.getElementById('myHtml').innerHTML
 
 export default () => {
   return <Home />
