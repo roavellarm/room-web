af94fc4a5d27623c125c4d280f72a27f74e4364f
Rodrigo Avellar <roavellarm@gmail.com>
2019-11-12 16:55:46 -0200

    Remove contact

--- src/components/Contact.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Button, Layout } from './templates'
-
-export default ({ showMessage }) => {
-  const content = <Button onClick={() => showMessage()}>Show message</Button>
-
-  return <Layout title={'Contact'} content={content} />
-}
--- src/containers/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Contact from '../components/Contact'
-
-export default () => {
-  const showMessage = () => {
-    alert('Hi!!!')
-  }
-  return <Contact showMessage={showMessage} />
-}
