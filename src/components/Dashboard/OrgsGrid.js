7b1586f16bf7fc2351d6c3800811be5f403daa72
Rodrigo Avellar <roavellarm@gmail.com>
2019-11-18 03:30:13 -0200

    Fix OrgsGrid

--- src/components/Dashboard/OrgsGrid.js
+++ src/components/Dashboard/OrgsGrid.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import Button from '@material-ui/core/Button'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import Container from '@material-ui/core/Container'
-import { makeStyles } from '@material-ui/core'
 import { Link } from 'react-router-dom'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  Container,
+  makeStyles,
+} from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
   orgGrid: {
     backgroundColor: theme.palette.background.paper,
     paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(8),
+    paddingBottom: theme.spacing(2),
   },
   org: {
     height: '100%',
@@ -50,7 +52,7 @@ export default ({ orgs }) => {
                 <Typography>{org.description}</Typography>
               </CardContent>
               <CardActions>
-                <Link to={`/org/${org.id}`}>
+                <Link to={`/org/${org.id}`} style={{ textDecoration: 'none' }}>
                   <Button size="small" color="primary">
                     Enter org
                   </Button>
