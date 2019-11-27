e8e1b5959ea810d74ce896da58ffc1cf2b590eb4
Rodrigo Avellar <roavellarm@gmail.com>
2019-11-27 04:31:40 -0200

    update chat api

--- src/api/chat.js
+++ src/api/chat.js
@@ -3,6 +3,6 @@ import axios from './'
 const base = '/chat'
 
 export const sendMessage = (room_id, message) =>
-  axios.post(`${base}`, { message, room_id })
+  axios.post(base, { message, room_id })
 
 export const getMessages = roomId => axios.get(`${base}/${roomId}`)
