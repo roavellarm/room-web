31d45c41aab3f279bb31c6da070cd54b1dd69671
Rodrigo Avellar <roavellarm@gmail.com>
2019-11-28 19:49:45 -0200

    Update form

--- src/components/templates/Form.js
+++ src/components/templates/Form.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Layout, HeroSection, TextField, InputFile } from './'
+import { Button, Layout, HeroSection, TextField, InputFile, Select } from './'
 import { Container } from '@material-ui/core'
 
 export default ({
@@ -10,31 +10,46 @@ export default ({
   sectionSubtitle,
   buttonTitle,
 }) => {
+  const renderField = item => {
+    if (item.type === 'file') return <InputFile />
+
+    if (item.type === 'select')
+      return <Select autoFocus={item.autoFocus}
+      key={item.name}
+      id={item.name}
+      name={item.name}
+      value={item.field}
+      required={item.required}
+       onChange={onChangeField} 
+       options={item.options} 
+       optionText='text' 
+       optionValue='value'
+        />
+
+    return (
+      <TextField
+        autoFocus={item.autoFocus}
+        key={item.name}
+        id={item.name}
+        required={item.required}
+        name={item.name}
+        type={item.type}
+        value={item.field}
+        label={item.label}
+        multiline={item.multiline}
+        onChange={onChangeField}
+        rows={item.rows}
+      />
+    )
+  }
+
   return (
     <Layout
       content={
         <>
           <HeroSection title={sectionTitle} subtitle={sectionSubtitle} />
           <Container align="left" component="main" maxWidth="xs">
-            {list.map(item =>
-              item.type === 'file' ? (
-                <InputFile />
-              ) : (
-                <TextField
-                  autoFocus={item.autoFocus}
-                  key={item.name}
-                  id={item.name}
-                  required={item.required}
-                  name={item.name}
-                  type={item.type}
-                  value={item.field}
-                  label={item.label}
-                  multiline={item.multiline}
-                  onChange={onChangeField}
-                  rows={item.rows}
-                />
-              ),
-            )}
+            {list.map(item => renderField(item))}
             <Button onClick={() => onSubmit()}>{buttonTitle}</Button>
           </Container>
         </>
