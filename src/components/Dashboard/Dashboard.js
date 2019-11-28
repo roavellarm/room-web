9ddc2a0abd9468bc556868d712930160b496bc80
Rodrigo Avellar <roavellarm@gmail.com>
2019-11-28 19:46:56 -0200

    Fix grid bug

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,40 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import { Grid, Tooltip, Fab, makeStyles } from '@material-ui/core'
+import { Tooltip, Fab } from '@material-ui/core'
 import AddIcon from '@material-ui/icons/Add'
 import { Layout } from '../templates'
 import { WithOrgs, WithoutOrg } from '.'
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    position: 'absolute',
-    bottom: '62px',
-  },
-  links: {
-    textDecoration: 'none',
-  },
-  avatar: {
-    color: '#fff',
-    backgroundColor: theme.palette.primary.main,
-  },
-}))
+const StyledFloatingButton = styled.div`
+  position: fixed;
+  right: 30px;
+  bottom: 70px;
+`
 
 export default ({ orgs }) => {
-  const classes = useStyles()
 
   const content = (
     <>
       {!orgs.length ? <WithoutOrg /> : <WithOrgs orgs={orgs} />}
-      <Grid
-        container
-        // direction="row"
-        justify="flex-end"
-        alignItems="flex-end"
-        className={classes.container}
-        spacing={2}
-      >
-        <Grid item>
-          <Link to={`/create-org`} style={{ textDecoration: 'none' }}>
+
+         <StyledFloatingButton>
+         <Link to={`/create-org`} style={{ textDecoration: 'none' }}>
             <Tooltip title="Create your org">
               <Fab
                 size="small"
@@ -45,9 +30,9 @@ export default ({ orgs }) => {
                 <AddIcon />
               </Fab>
             </Tooltip>
-          </Link>
-        </Grid>
-      </Grid>
+            </Link>
+            </StyledFloatingButton>
+
     </>
   )
   return <Layout content={content} />
